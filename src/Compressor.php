d85c1135d3c24a76221e5d2cde6f6dac040afb5f
Adam Nicholson <adamnicholson10@gmail.com>
2015-12-20 18:08:01 +0000

    Dynmaically determine words to replace with short keys

--- src/Compressor.php
+++ src/Compressor.php
@@ -4,13 +4,77 @@ namespace Adamnicholson\Squeeze;
 
 class Compressor
 {
+    private $usedKeys = [];
+
     public function compress(string $data): string
     {
-        return str_replace('Vitae', '__V', $data);
+        $replacements = [];
+
+        preg_match_all('/\s([a-zA-Z0-0]+)\s/', $data, $matches);
+
+        $words = array_unique($matches[1]);
+
+        foreach ($words as $word) {
+            $key = $this->findKey($data);
+
+            if (strlen($word) <= strlen($key)) {
+                // Do not bother if the key is as big as the word
+                $this->freeKey($key);
+                continue;
+            }
+
+            $usages = substr_count($data, $word);
+
+            $usedSpace =  $usages * strlen($word);
+            $willUse = $usages * strlen($key) + strlen($key) + strlen($word) + 1;
+            if ($willUse >= $usedSpace) {
+                // Do not bother if the extra meta data means its overall larger with replacement
+                $this->freeKey($key);
+                continue;
+            }
+
+            $replacements[$word] = $key;
+        }
+
+        //@ todo Not sure why it fails if we don't have a final random replace on the end
+        $replacements[")(*&"] = $this->findKey($data);
+
+        $data = str_replace(array_keys($replacements), $replacements, $data);
+
+        $meta = http_build_query($replacements);
+
+        return $meta . PHP_EOL . $data;
     }
 
-    public function decompresse(string $compressed): string
+    public function decompress(string $compressed): string
     {
-        return str_replace('__V', 'Vitae', $compressed);
+        preg_match('/^.+\n/', $compressed, $meta);
+
+        parse_str($meta[0], $replacements);
+
+        $body = preg_replace('/^.+\n/', '', $compressed);
+
+        $body = str_replace(array_values($replacements), array_keys($replacements), $body);
+
+        return $body;
+    }
+
+    private function findKey(string $data): string
+    {
+        $i = 0;
+        $literal = '_' . $i . '_';
+        while (strstr($data, $literal) || in_array($literal, $this->usedKeys)) {
+            $literal = '_' . $i . '_';
+            $i++;
+        }
+
+        $this->usedKeys[$literal] = $literal;
+
+        return $literal;
+    }
+
+    private function freeKey(string $key)
+    {
+        unset($this->usedKeys[$key]);
     }
 }
